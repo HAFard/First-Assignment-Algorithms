eb87508987fd69c81c8074f587b52ffb613179c9
HAFard <f.hananeh@gmail.com>
2024-02-25 15:33:50 +0330

    add twoSum function

--- src/main/java/Exercises2.java
+++ src/main/java/Exercises2.java
@@ -14,8 +14,23 @@ public class Exercises2 {
     */
 
     public int[] twoSum(int[] nums, int target) {
-        // TODO
-        return null;
+
+        int[] indexes = new int[2] ;
+
+        for(int i = 0 ; i < nums.length ; i++)
+        {
+            for (int j = i+1 ; j < nums.length; j++)
+            {
+                if (nums[i]+nums[j] == target)
+                {
+                    indexes[0] = i ;
+                    indexes[1] = j ;
+                    break;
+                }
+            }
+        }
+
+        return indexes;
     }
 
     /*
